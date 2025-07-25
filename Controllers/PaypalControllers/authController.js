fe74709a54c9c746eec1a6e9d3c92f43a5e48e36
unknown <sdjsm2003@gmail.com>
2025-07-25 13:05:44 -0400

    Practica1Paypal, Lista

---
+++ Controllers/PaypalControllers/authController.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const generateAccessToken = async () => {
+  try {
+    
+    const response = await axios({
+      url: process.env.PAYPAL_BASE_URL + "/v1/oauth2/token",
+      method: "post",
+      data: 'grant_type=client_credentials',
+      auth: {
+        username: process.env.PAYPAL_CLIENT_ID,
+        password: process.env.PAYPAL_SECRET_KEY,
+      },
+    });
+
+    return response != null && response?.data
+      ? response.data.access_token
+      : null;
+  } catch (error) {
+    
+    throw new Error(error.message);
+  }
+};
+
+module.exports = generateAccessToken;
---
+++ Handlers/PaypalHandler/createOrderHandler.js
@@ -0,0 +1,17 @@
+//import {Request, Response} from "express";
+const createOrder = require('../../Controllers/PaypalControllers/createOrder');
+
+const createOrderHandler = async(req , res)=>{
+    try {
+        if(!req.body) return res.status(400).json({status: "error", message: "We need the body to test this out."});
+
+        const answer = await createOrder(req.body);
+
+        return (answer != null) ? res.status(200).json(answer): res.status(400).json({status: "error", message: answer});
+    } catch (error) {
+        
+        return res.status(500).json(error?.message)
+    }
+};
+
+module.exports= createOrderHandler;
